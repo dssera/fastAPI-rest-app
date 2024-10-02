c6351d06c1cfd2f3f21a176906e36a1a8693222c
dssera <zykove08@gmail.com>
2024-10-02 17:19:45 +0300

    fix: add owner_id param in create_note()

--- src/crud.py
+++ src/crud.py
@@ -14,8 +14,8 @@ def get_note(db: Session, note_id: int):
     return db.query(models.Note).filter(models.Note.id == note_id).first()
 
 
-def create_note(db: Session, note: schemas.NoteCreate) -> models.Note:
-    db_note = models.Note(note.model_dump())
+def create_note(db: Session, note: schemas.NoteCreate, owner_id) -> models.Note:
+    db_note = models.Note(text=note.text, owner_id=owner_id)
     db.add(db_note)
     db.commit()
     db.refresh(db_note)
@@ -29,6 +29,13 @@ def get_users(db: Session, skip: int = 0, limit: int = 100) -> List[Type[models.
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(username=user.username, hashed_password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
-def get_user_by_email(db: Session, username: str):
+
+def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
