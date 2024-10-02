3cc5e51e6f7e31a719d5cb6e8b8abf8aad9e9b1e
dssera <zykove08@gmail.com>
2024-10-02 16:23:56 +0300

    add crud for Note and User

---
+++ src/crud.py
@@ -0,0 +1,34 @@
+from typing import List, Type
+
+from sqlalchemy.orm import Session
+
+from . import models
+from . import schemas
+
+
+def get_notes(db: Session, skip: int = 0, limit: int = 100) -> List[Type[models.Note]]:
+    return db.query(models.Note).offset(skip).limit(limit).all()
+
+
+def get_note(db: Session, note_id: int):
+    return db.query(models.Note).filter(models.Note.id == note_id).first()
+
+
+def create_note(db: Session, note: schemas.NoteCreate) -> models.Note:
+    db_note = models.Note(note.model_dump())
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return db_note
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100) -> List[Type[models.Note]]:
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
