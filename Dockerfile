4d87e589d9c2e0351ded6b81380b4158b578293e
dssera <adesser001@mail.ru>
2024-10-21 12:07:54 +0300

    switched from sqlite to postgres and added Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.9.13-slim-buster
+
 LABEL authors="Dessera"
 
 WORKDIR /usr/local/app
@@ -12,3 +13,5 @@ COPY src ./src
 EXPOSE 5000
 
 CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "5000"]
+# docker build -t notes-app-img .
+# docker run -d --name notes-app-c --network notes-net -e POSTGRES_PASSWORD=12345 -p 8080:5000 notes-app-img
--- src/database.py
+++ src/database.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+# to postgresql pattern - postgresql+psycopg2://user:password@host:port/dbname
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./itemsAppWithAuth.db"
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL='postgresql+psycopg2://postgres:12345@localhost:5432/notes_db_temp_temp'
+# with docker - must use container name
+SQLALCHEMY_DATABASE_URL='postgresql+psycopg2://postgres:12345@notes_app_postgres:5432/notes_db'
+# old db
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./itemsAppWithAuth.db"
+
+# https://stackoverflow.com/questions/54118182/sqlalchemy-not-creating-tables
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
